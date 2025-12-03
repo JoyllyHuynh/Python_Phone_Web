5b2a03ef4a5f91753fdf85e84de26de9ef7450c7
JollyHuynh <23130323@st.hcmuaf.edu.vn>
2025-12-03 14:35:55 +0700

    update

--- app/urls.py
+++ app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('checkout/', views.checkout, name='checkout'),
     path('update_item/', views.updateItem, name='update_item'),
+    path('product-detail/<int:pk>/', views.product_detail, name='product_detail'),
+    path('brand/<slug:brand_slug>/', views.product_list_by_brand, name='product_list_by_brand'),
 ]
