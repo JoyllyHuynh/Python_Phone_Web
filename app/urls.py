4e1752806750cb63d5697406f9848f6798814313
Võ Nhật Tân <23130293@st.hcmuaf.edu.vn>
2025-12-28 20:02:56 +0700

    seach

--- app/urls.py
+++ app/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('product_search/', views.product_search, name='product_search'),
+
     path('promotion_list/', views.promotion_list, name='promotion_list'),
     
 
